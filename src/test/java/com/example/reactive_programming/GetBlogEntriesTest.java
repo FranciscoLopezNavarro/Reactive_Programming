c175e538075ed3d28f67508a6f1dda353cd70bfa
Francisco Lopez Navarro <francisco.lopez.navarro@kairosds.com>
2022-10-02 19:50:47 +0200

    test: Create Basic Get Blog Entries test.

--- src/test/java/com/example/reactive_programming/GetBlogEntriesTest.java
+++ src/test/java/com/example/reactive_programming/GetBlogEntriesTest.java
@@ -1,13 +1,41 @@
 package com.example.reactive_programming;
 
+import com.example.reactive_programming.domain.model.Entry;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GetBlogEntriesTest {
 
+    @Autowired
+    private WebTestClient webClient;
+
+
     @Test
-    void contextLoads() {
+    @DisplayName("Should return empty list if no entries")
+    void shouldReturnEmptyListIfNoEntries() {
+        webClient.get().uri("/blog/entries")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(APPLICATION_JSON_VALUE)
+                .expectBodyList(Entry.class);
     }
+
+
+    @Test
+    @DisplayName("Should return categories if they exists categories")
+    void shouldGetCategories() {
+        webClient.get().uri("/blog/entries")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(APPLICATION_JSON_VALUE)
+                .expectBodyList(Entry.class);
+    }
+
 
 }
